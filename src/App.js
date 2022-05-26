45f783bd737d7505f4e528d6df7adfb583e219d1
cr1m3s <parioleks@gmail.com>
2022-05-26 15:27:30 +0200

    1st ex

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Profile from './components/Profile';
+import user from './user.json';
+
+function App() {
+  return (
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+}
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import './Profile.css';
+
+export default function Profile(props) {
+  const { avatar, username, tag, location, stats } = props;
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number),
+};
